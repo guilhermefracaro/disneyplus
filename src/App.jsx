bd58923b12adb1768c560a7b80a65ddb03fec46e
guilhermefracaro <guifracaro@outlook.com>
2024-04-03 15:19:08 -0300

    disney+

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header'
+import ProductionHouse from './components/ProductionHouse'
+import GenreMovieList from './components/GenreMovieList'
+
+function App() {
+  return (
+    <div className=''>
+      <Header />
+      <ProductionHouse />
+      <GenreMovieList />
+    </div>
+  )
+}
+
+export default App
